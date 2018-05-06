fb93f7ed75c5214ac13e8795cef22fd7f0910235
hecgarri <hectorgarridohenriquez@gmail.com>
2018-05-06 04:25:00 -0300

    Ordené un poco el script y utilicé algunos contrastes nuevos.

--- rutina_ologit.R
+++ rutina_ologit.R
@@ -1,68 +1,46 @@
-if (!require(foreign)) install.packages("foreign"); require(foreign) ## Importar bases de datos en otros formatos 
-if (!require(ggplot2)) install.packages("ggplot2"); require(ggplot2)## Para gráficas más sofisticadas 
-if (!require(MASS)) install.packages("MASS"); require(MASS)
-if (!require(Hmisc)) install.packages("Hmisc"); require(Hmisc)
-if (!require(reshape2)) install.packages("reshape2"); require(Hmisc) # Para dar formato a las data frames 
-if (!require(dplyr)) install.packages("dplyr"); require(dplyr) # para el operador %>% 
-if (!require(VGAM)) install.packages(VGAM); require(VGAM) # Modelos ordinales
-
 rm(list = ls())
-#dat <- read.dta("https://stats.idre.ucla.edu/stat/data/ologit.dta")
+
 setwd("/home/hector/GoogleDrivePersonal/Docencia/Tesistas/Rodrigo Monje /")
 
+
+if (!require(foreign)) install.packages("foreign"); require(foreign) ## Importar bases de datos en otros formatos 
+if (!require(ggplot2)) install.packages("ggplot2"); require(ggplot2)## Para gráficas más sofisticadas 
+if (!require(tidyverse)) install.packages("tidyverse"); require(tidyverse) # para el operador %>% 
+if (!require(quantreg)) install.packages("quantreg"); require(quantreg)
+if (!require(stargazer)) install.packages("stargazer"); require(stargazer)
+if (!require(sandwich)) install.packages("sandwich"); require(sandwich)
+if (!require(lmtest)) install.packages("lmtest"); require(lmtest)
+# Se quitan los datos de vinos de mendoza y de españa. 
 data = read.csv("datos_vino_MEJORADA.csv") %>% filter(valle!="MENDOZA", valle!="ESPAÑA")
  
- # 80-84 : correcto o bueno 
- # 85-86: buenos aspirando a muy buenos 
- # 87-88: Muy buenos 
- # 89-90: Vinos que sorprenden 
- # 91-92: Excelente, con clase 
- # 93-94: Vinos sublimes 
- # >95: camino a la perfección 
 
-data = mutate(data, tramos = ifelse(puntaje>=80 & puntaje<=84,1, 
-                             ifelse(puntaje>=85 & puntaje<=86,2,
-                             ifelse(puntaje>=87 & puntaje<=88,3, 
-                             ifelse(puntaje>=89 & puntaje<=90,4,
-                             ifelse(puntaje>=91 & puntaje<=92,5,
-                             ifelse(puntaje>=93 & puntaje<=94,6, 
-                             ifelse(puntaje>=95,7,NA)))))))) 
+data = mutate(data,
+      tramos = ifelse(puntaje>=80 & puntaje<=84,1,  # 80-84 : correcto o bueno 
+      ifelse(puntaje>=85 & puntaje<=86,2, # 85-86: buenos aspirando a muy buenos 
+      ifelse(puntaje>=87 & puntaje<=88,3,  # 87-88: Muy buenos  
+      ifelse(puntaje>=89 & puntaje<=90,4,  # 89-90: Vinos que sorprenden 
+      ifelse(puntaje>=91 & puntaje<=92,5,  # 91-92: Excelente, con clase 
+      ifelse(puntaje>=93 & puntaje<=94,6,  # 93-94: Vinos sublimes 
+      ifelse(puntaje>=95,7,NA))))))), # >95: camino a la perfección
+      tramos2 = ifelse(tramos==1 | tramos==2,1, 
+      ifelse(tramos==3,2, 
+      ifelse(tramos==4,3, 
+      ifelse(tramos==5,4, 
+      ifelse(tramos==6 | tramos==7,5,NA))))),
+      puntajesq = puntaje^2)   
 
-data = mutate(data, tramos2 = ifelse(tramos==1 | tramos==2,1, 
-                              ifelse(tramos==3,2, 
-                              ifelse(tramos==4,3, 
-                              ifelse(tramos==5,4, 
-                              ifelse(tramos==6 | tramos==7,5,NA))))))  
-table(data$tramos2)
+data = mutate(data, 
+              precio2 = ifelse(pmercado>=2990 & pmercado<=6999,1, # Reserva entre 4.000 a 6.999
+                        ifelse(pmercado>=7000 & pmercado<=11999,2,  # Gran reserva 7.000  a 11.999
+                        ifelse(pmercado>=12000 & pmercado<=19999,3,  # Premium de 12.000 a 19.999
+                        ifelse(pmercado>=20000 & pmercado<=39999,4, # Super premium 20.000 a 39.999
+                        ifelse(pmercado>=40000,5,NA))))))  # Ícono >40.000
 
- # Reserva entre 4.000 a 6.999
- # Gran reserva 7.000  a 11.999
- # Premium de 12.000 a 19.999
- # Super premium 20.000 a 39.999
- # Ícono >40.000
-
-data = mutate(data, precio2 = ifelse(pmercado>=2990 & pmercado<=6999,1,
-              ifelse(pmercado>=7000 & pmercado<=11999,2,
-              ifelse(pmercado>=12000 & pmercado<=19999,3,
-              ifelse(pmercado>=20000 & pmercado<=39999,4,
-              ifelse(pmercado>=40000,5,NA))))))
-
-
-data = mutate(data, p_r = ifelse(precio2==1,1,0))
-data = mutate(data, p_gr = ifelse(precio2==2,1,0))
-data = mutate(data, p_p = ifelse(precio2==3,1,0))
-data = mutate(data, p_sp = ifelse(precio2==4,1,0))
-data = mutate(data, p_i = ifelse(precio2==5,1,0))
-
-
-data$tramos2 = factor(data$tramos2, ordered=TRUE) 
-data$precio2 = factor(data$precio2, ordered=TRUE) 
-
-
-prop.table(table(data$tramos))
-
-
-ftable(xtabs(~pmercado+tramos, data=data))
+data = mutate(data, p_r = ifelse(precio2==1,1,0)) # Precio reserva
+data = mutate(data, p_gr = ifelse(precio2==2,1,0)) #Precio gran reserva
+data = mutate(data, p_p = ifelse(precio2==3,1,0)) #Precio premium 
+data = mutate(data, p_sp = ifelse(precio2==4,1,0)) #Precio super premium 
+data = mutate(data, p_i = ifelse(precio2==5,1,0)) # Precio ícono
 
 data = mutate(data, cs = ifelse(cepa=="CABERNET SAUVIGNON",1,0))
 data = mutate(data, ca = ifelse(cepa=="CARMENERE",1,0))
@@ -80,14 +58,8 @@ data = mutate(data, oc = ifelse(cepa=="CABERNET FRANC" |
                                   cepa=="PINOT NOIR" |
                                   cepa=="ZINFANDEL ",1,0))
 
-
-data = mutate(data, csca = ifelse(cs==1 | ca==1,1,0))
-
-data = mutate(data, came = ifelse(ca==1 | me==1,1,0))
-
-
-
-data$precio2 = factor(data$precio2, ordered = TRUE)
+data = mutate(data, csca = ifelse(cs==1 | ca==1,1,0)) #cabernet y carmenere
+data = mutate(data, came = ifelse(ca==1 | me==1,1,0)) #cabernet y merlot
 
 data = mutate(data, NORTE=ifelse(is.na(NORTE),0,1))
 data = mutate(data, CENTRO=ifelse(is.na(CENTRO),0,1))
@@ -101,133 +73,69 @@ data = mutate(data, organolepticas = (astringencia+
 data = mutate(data, colchagua = ifelse(valle=="COLCHAGUA" |
                                          valle =="COLCHAGUA COSTA"|
                                          valle =="ALTO COLCHAGUA",1,0), 
-              antigüedad = 2016-año)
+              antigüedad = 2016-año, 
+              antisq = antigüedad^2)
 
-
-m11 <- polr(precio2 ~ gran.reserva+super.premium+premium+icono+
-            csca+me+sy+et+puntaje_sm+I(puntaje_sm^2)+
-              astringencia+frescor+dulzura+cuerpo+medianas.bajo.precio+
-              emergentes.exclusivas+valle.maipo+colchagua+
-              antigüedad,
-            data=data, Hess=TRUE)
-m12 = polr(precio2~1, data=data, Hess=TRUE)
-
-1-(logLik(m11)/logLik(m12))
-
-(ctable1 <- coef(summary(m11)))
-
-p1 <- pnorm(abs(ctable1[, "t value"]), lower.tail = FALSE)*2
-
-## combined table
-(ctable1 <- cbind(ctable1, "p value" = round(p1, digits=2)))
-
-data = mutate(data, precio_sm =  pmercado-14613.18)
-
-m21 = polr(tramos2 ~ gran.reserva +premium+ super.premium + icono + csca + me + 
-                   sy + et + astringencia + frescor + cuerpo + dulzura+
-              medianas.bajo.precio+emergentes.exclusivas+
-              antigüedad+I(antigüedad^2)+p_gr+p_p+p_sp+p_i+
-              valle.maipo+colchagua,
-                 data=data, Hess=TRUE)
-
-phat = predict(m21, type="probs")
-
-write.csv(phat, "simulacion_precios.csv")
-
-(ctable <- coef(summary(m21)))
-
-p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE)*2
-
-## combined table
-(ctable <- cbind(ctable, "p value" = round(p, digits=2)))
-
-m22 = polr(tramos2 ~ 1,
-           data=data, Hess=TRUE)
-
-r2 = 1-(logLik(m21)/logLik(m22))
-
-#####################################################################################################
-
-if (!require(quantreg)) install.packages("quantreg")
-if (!require(stargazer)) install.packages("stargazer")
-
+#Estamos excluyendo otras cepas 
 
 data = data %>% filter(oc!=1)
 
-modelo = rq(log(pmercado) ~ gran.reserva+premium+super.premium+icono+
-     cs+came+sy+et+
-     medianas.bajo.precio+astringencia+frescor+cuerpo+dulzura+
-     emergentes.exclusivas+valle.maipo+colchagua+
-     antigüedad+I(antigüedad**2)+I(emergentes.exclusivas*super.premium)+
-       I(medianas.bajo.precio*et)+
-       I(medianas.bajo.precio*gran.reserva)+
-       I(colchagua*icono)+I(colchagua*gran.reserva)+
-       I(colchagua*premium),tau = .5, data=data)
-summary(modelo, se = "boot", bsmethod = "xy", R = 1000)
+form = "log(pmercado) ~ gran.reserva+premium+super.premium+icono+
+  cs+came+sy+et+
+  medianas.bajo.precio+emergentes.exclusivas+
+  valle.maipo+colchagua+
+  puntaje+I(puntaje**2)+
+  antigüedad+I(antigüedad**2)"
 
-stepwise = stepAIC(modelo, direction = c("both"))
+ols = lm(form, data = data)
+coeftest(ols, vcov = vcovHC(ols, "HC1"))
 
-summary(stepwise)
+modelo1 = rq(form,tau = .25, data=data, method = "br", 
+            contrasts = TRUE)
+modelo2 = rq(form,tau = .5, data=data, method = "br", 
+             contrasts = TRUE)
+modelo3 = rq(form,tau = .75, data=data, method = "br", 
+             contrasts = TRUE)
 
-modelo1 = rq(formula(stepwise), tau = .5, data = data)
+rho = function(u, tau=.5) u*(tau-(u<0))
+
+V_hat1 = sum(rho(modelo1$residuals, modelo1$tau)) 
+V_tilde1 = sum(rho(rq(log(data$pmercado)~1, tau = .25,
+                      method = "fn")$residuals,
+                   modelo1$tau))
+  
+R1 = 1-(V_hat1/V_tilde1)  
+
+V_hat2 = sum(rho(modelo2$residuals, modelo2$tau)) 
+V_tilde2 = sum(rho(rq(log(data$pmercado)~1, tau = .5,
+                      method = "fn")$residuals,
+                   modelo2$tau))
+
+R2 = 1-(V_hat2/V_tilde2)  
+
+
+V_hat3 = sum(rho(modelo3$residuals, modelo3$tau)) 
+V_tilde3 = sum(rho(rq(log(data$pmercado)~1, tau = .75,
+                      method = "fn")$residuals,
+                   modelo3$tau))
+
+R3 = 1-(V_hat3/V_tilde3)  
+
 sumM1 = summary(modelo1, se = "boot", bsmethod = "xy", R = 1000)
-
-modelo2 = rq(formula(stepwise),tau = .25, data=data)
 sumM2 = summary(modelo2, se = "boot", bsmethod = "xy", R = 1000)
-
-modelo3 = rq(formula(stepwise),tau = .75,data=data)
 sumM3 = summary(modelo3, se = "boot", bsmethod = "xy", R = 1000)
 
-QR = rq(formula(stepwise),tau = seq(0.05,0.95, by=0.05), data=data)
-sumQR = summary(QR, se = "boot", bsmethod = "xy", R = 1000)
+stargazer(sumM1$coefficients, type = "text")
+stargazer(sumM2$coefficients, type = "text")
+stargazer(sumM3$coefficients, type = "text")
 
-plot(sumQR, ols = FALSE)
 
 
-############################################################
-############################################################
-
-modelo = rq(log(puntaje) ~ gran.reserva+premium+super.premium+icono+
-              cs+came+sy+et+
-              medianas.bajo.precio+astringencia+frescor+cuerpo+dulzura+
-              emergentes.exclusivas+valle.maipo+colchagua+cuma+
-              antigüedad+I(antigüedad**2)+I(emergentes.exclusivas*super.premium)+
-              I(medianas.bajo.precio*et)+
-              I(medianas.bajo.precio*gran.reserva)+
-              I(colchagua*icono)+I(colchagua*gran.reserva)+
-              I(colchagua*premium),tau = .5, data=data)
-summary(modelo, se = "boot", bsmethod = "xy", R = 1000)
-
-stepwise = stepAIC(modelo, direction = c("both"))
-
-summary(stepwise)
-
-form = "log(puntaje) ~ gran.reserva + premium + super.premium + icono + 
-    sy + astringencia + frescor + cuerpo + antigüedad + I(antigüedad^2) + 
-    I(colchagua * icono)+medianas.bajo.precio+emergentes.exclusivas+premium*et+
-colchagua*premium"
-
-modelo1 = rq(formula(stepwise), tau = .5, data = data)
-sumM1 = summary(modelo1, se = "boot", bsmethod = "xy", R = 1000)
-
-modelo2 = rq(formula(stepwise),tau = .25, data=data)
-sumM2 = summary(modelo2, se = "boot", bsmethod = "xy", R = 100)
-
-modelo3 = rq(formula(stepwise),tau = .75,data=data)
-sumM3 = summary(modelo3, se = "boot", bsmethod = "xy", R = 100)
-
-QR = rq(form,tau = seq(0.05,0.95, by=0.05), data=data)
+QR = rq(form,tau = seq(0.05,0.95, by=0.05), data=data, method = "fn")
 sumQR = summary(QR, se = "boot", bsmethod = "xy", R = 100)
 
-plot(sumQR, ols = FALSE)
+plot(sumQR, parm = c(-1,-6,-7,-8,-9),ols = FALSE)
 
+summary(ols$model)
 
-modelo = lm(form, data = data)
-
-require(car)
-
-
-
-
-
-
+anova(modelo1,modelo2, modelo3, test = "rank", joint = FALSE)
